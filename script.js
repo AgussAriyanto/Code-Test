f1bf1ae750e158e4a0cdfc6be33c7d39bdd9f281
Abd. Saleh <79849643+AbdSalehh@users.noreply.github.com>
2022-11-09 17:24:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+<<<<<<< main
+// Javascript code
 console.log("Test")
 console.log("Hello World")
+
+function main() {
+return null;
+}
+main();
+
+// console.log(main);
+=======
+console.log("Test")
+console.log("Hello World")
+>>>>>>> main
